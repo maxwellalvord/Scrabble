a48bb01e1905276292ffdc3adc834b749158fd17
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-14 11:51:22 -0700

    add second pass with full word
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>

--- Scrabble/Model/Scrabble.cs
+++ Scrabble/Model/Scrabble.cs
@@ -2,21 +2,50 @@
 using System.Collections.Generic;
 
 
+
 namespace Scrabble.Models
 {
   public class Word
   {
     public string Content { get; set;}
+    
 
-    public string Letter (string word)//c // length = 1
+    public int Letter (string word)//c // length = 1
     {
-      // string l = word;
-      // int Score = 0;
-      // for ( int i = 0; i <= l.Length -1 ; i++)
-      // {
-       
-      // }
-      return word;
+      int Score = 0;
+      for ( int i = 0; i <= word.Length -1 ; i++)
+      {
+        
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' || word[i] == 'l' || word[i] == 'n' || word[i] == 'r' || word[i] == 's' || word[i] == 't')
+        {
+          Score += 1;
+        }
+        else if (word[i] == 'd' || word[i] ==  'g')
+        {
+          Score += 2;
+        }
+        else if (word[i] == 'b' || word[i] ==  'c' || word[i] ==  'm' || word[i] ==  'p')
+        {
+          Score += 3;
+        }
+        else if (word[i] == 'f' || word[i] ==  'h' || word[i] ==  'v' || word[i] ==  'w' || word[i] == 'y')
+        {
+          Score += 4;
+        }
+        else if (word[i] == 'k' )
+        {
+          Score += 5;
+        }
+        else if (word[i] == 'j' || word[i] == 'x')
+        {
+          Score += 8;
+        }
+        else 
+        {
+          Score += 10;
+        }
+      }
+      return Score;
     }
     // private static List<Word> _instances = new List<Word> {};
 
